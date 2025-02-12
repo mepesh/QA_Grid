5668a8b36e70abfc9bf333f1f68ac4ed9f0517f1
BScIT <dipesh.pandey42@gmail.com>
2025-02-12 15:24:57 +0545

    fa

--- src/test/java/grid_test.java
+++ src/test/java/grid_test.java
@@ -1,6 +1,21 @@
+import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.BeforeTest;
+
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class grid_test {
 
-    WebDriver driver;
+    @BeforeTest
+    public void SetGrid() throws MalformedURLException {
+        String url = "http://localhost:4444/wd/hub";
+        DesiredCapabilities cap = new DesiredCapabilities();
+        cap.setPlatform(Platform.WIN10);
+        WebDriver driver = new RemoteWebDriver(new URL(url), cap);
+
+    }
+
 }
