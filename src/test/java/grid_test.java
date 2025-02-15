3ed8c804429440a76d42302b0356f088614d53bd
unknown <dipesh.pandey42@gmail.com>
2025-02-15 15:36:07 +0545

    Checing grid setup

--- src/test/java/grid_test.java
+++ src/test/java/grid_test.java
@@ -8,13 +8,18 @@ import java.net.MalformedURLException;
 import java.net.URL;
 
 public class grid_test {
+    //https://www.youtube.com/watch?v=XB550hmwzLM&t=2688s
+    //https://www.youtube.com/watch?v=dfnVAAw6PV4&t=942s
 
     @BeforeTest
     public void SetGrid() throws MalformedURLException {
         String url = "http://localhost:4444/wd/hub";
         DesiredCapabilities cap = new DesiredCapabilities();
         cap.setPlatform(Platform.WIN10);
+        cap.setBrowserName("chrome");
         WebDriver driver = new RemoteWebDriver(new URL(url), cap);
+
+
 
     }
 
